d103ed995c29ea04775241ef9fbe1db8a9d7d617
Philipp korber <philippkorber@gmail.com>
2016-09-09 20:01:57 +0200

    Added more examples, extended README

--- README.md
+++ README.md
@@ -5,12 +5,56 @@
 
 ---
 
-TODO
+Fuseflag provides a Fuse/Flag which can be shared (by cloning) between multiple threads.
+The Fuse/Flag can be checked and burned, once burned it cannot be restored.
+
+A additional functionality is a wrapper around `thread::spawn` which takes a closure
+expecting a `FuseFlag` as single parameter and returns a Wrapper  around `JoinHandle`
+which burns the fuse befor calling join.
 
 ## Example
 
 ```rust
-TODO
+extern crate fuseflag;
+
+use std::time::Duration;
+use std::thread::{sleep, spawn, JoinHandle};
+
+use fuseflag::FuseFlag;
+
+fn background_job(end_flag: FuseFlag, id: u64) -> JoinHandle<()> {
+    spawn(move || {
+        while end_flag.check() {
+            println!("\tbg-job({}): continues workin", id);
+            //Background work Work
+            sleep(Duration::from_millis(430*id));
+        }
+        println!("\tbg-job({}) stops running", id);
+    })
+}
+
+fn main() {
+    let end_fuse = FuseFlag::new();
+
+    let bg_guard = vec![
+        background_job(end_fuse.clone(), 1),
+        background_job(end_fuse.clone(), 2)
+    ];
+
+
+    println!("running fake ui loop...");
+    //naturally a very nice ui called sleep ;)
+    sleep(Duration::from_secs(3));
+    println!("main loop ended: waiting for background jobs to stop");
+
+    //this will trigger .check() to return false (for ever from now on)
+    end_fuse.burn();
+
+    //join the threads
+    for guard in bg_guard {
+        guard.join().unwrap();
+    }
+}
 ```
 
 ## License
--- examples/example1.rs
+++ examples/example1.rs
@@ -1,67 +1,40 @@
-/// A (stupid??) example for using fuseflag
-///
-/// This will run two threads on wich does "fake" loads,
-/// and one which reads from stdin. The background loop
-/// has to fuses, one which is burn when the user enters
-/// "fast", which will change the bg-jobs behaviour. The
-/// other is implicitly burnd when the user presses "j" (for exit)
-/// and the main thread waits/joins the background thread.
-/// This fuse is also implicitly passed to the bg-job and is
-/// used to signal it to stop.fa
 extern crate fuseflag;
 
-use std::thread::{sleep};
 use std::time::Duration;
-use std::io;
-use std::io::prelude::*;
+use std::thread::{sleep, spawn, JoinHandle};
 
-use fuseflag::{FuseFlag, StopThreadFuse, fused_spawn};
+use fuseflag::FuseFlag;
 
-fn background_job(not_crazy: FuseFlag) -> StopThreadFuse<()> {
-    fused_spawn(move |do_continue| {
-        while do_continue.check() && not_crazy.check() {
-            // do some work, e.g. update sql database
-            sleep(Duration::from_secs(2));
-            println!("another workload done...")
-        };
-        while do_continue.check() {
-            sleep(Duration::from_millis(600));
-            println!("..so much work...");
-            println!("another workload done...")
+fn background_job(end_flag: FuseFlag, id: u64) -> JoinHandle<()> {
+    spawn(move || {
+        while end_flag.check() {
+            println!("\tbg-job({}): continues workin", id);
+            //Background work Work
+            sleep(Duration::from_millis(430*id));
         }
+        println!("\tbg-job({}) stops running", id);
     })
 }
 
-fn main_loop(speed_fuse: FuseFlag) {
-    let stdin = io::stdin();
-    print!("\nexit [j/n]? ");
-    for line in stdin.lock().lines() {
-        match line {
-            Ok(text) => {
-                println!("you said: {}", &*text);
-                if text == "j" {
-                    break;
-                } else if text == "fast" {
-                    println!("nooo! It might be faster?!?");
-                    speed_fuse.burn();
-                }
-            },
-            Err(err) => {
-                println!("ups: {:?}", err);
-                break;
-            }
-        }
-        print!("\nexit [j/n]? ");
-    }
-}
-
 fn main() {
-    let fuse = FuseFlag::new();
-    let bg_guard = background_job(fuse.clone());
+    let end_fuse = FuseFlag::new();
 
-    main_loop(fuse);
+    let bg_guard = vec![
+        background_job(end_fuse.clone(), 1),
+        background_job(end_fuse.clone(), 2)
+    ];
 
-    println!("Waiting for background job to exist");
-    bg_guard.stop_and_join().expect("no panic in background job");
-    println!("Good by ;-)");
+
+    println!("running fake ui loop...");
+    //naturally a very nice ui called sleep ;)
+    sleep(Duration::from_secs(3));
+    println!("main loop ended: waiting for background jobs to stop");
+
+    //this will trigger .check() to return false (for ever from now on)
+    end_fuse.burn();
+
+    //join the threads
+    for guard in bg_guard {
+        guard.join().unwrap();
+    }
 }
